5696d8dd5124afcb4656200022ef5fd15bd82fd4
Franck <franckvienot7@gmail.com>
2023-08-29 00:23:14 +0200

    corrected one esLint error and commented my custom decorator function

--- src/auth/decorator/get-user.decorator.ts
+++ src/auth/decorator/get-user.decorator.ts
@@ -1,5 +1,6 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
+// Here I had created a custom decorator to call easily the user informations
 export const GetUser = createParamDecorator(
   (data: string | undefined, ctx: ExecutionContext) => {
     const request: Express.Request = ctx.switchToHttp().getRequest();
