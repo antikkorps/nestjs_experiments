15584ec060405256616104cc9dff3d629e873b2a
Franck <franckvienot7@gmail.com>
2023-11-21 23:34:16 +0100

    add frontend url to origin

--- dist/src/main.js
+++ dist/src/main.js
@@ -6,7 +6,7 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     const corsOptions = {
-        origin: 'http://localhost:5173',
+        origin: ['http://localhost:5173', 'https://garagefront.fvienot.link/'],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true,
     };
