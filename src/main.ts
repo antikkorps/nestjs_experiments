290a98a1865e232b3ceba0d755052e63b4a56c9e
Franck Vienot <68828345+antikkorps@users.noreply.github.com>
2024-02-14 12:57:14 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    origin: ['http://localhost:5173', 'https://garagefront.fvienot.link/'], // URL front-end
+    origin: ['*'], // URL front-end
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Permettre les cookies
   };
